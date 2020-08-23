9896936a6dd02e155dc33926b2657116894e9b4d
chrisimi <mittermaier.c@a1.net>
2020-08-23 14:42:16 +0200

    added a test for the casino plugin

--- src/test/java/VersionCheckerTests.java
+++ src/test/java/VersionCheckerTests.java
@@ -1,10 +1,18 @@
-import org.junit.jupiter.api.Test;
+package test.java;
+
+import com.chrisimi.versionchecker.VersionChecker;
+import com.chrisimi.versionchecker.VersionResult;
+import com.chrisimi.versionchecker.VersionStatus;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class VersionCheckerTests
 {
     @Test
     public void TestWithCasinoPlugin()
     {
-
+        VersionResult result = VersionChecker.getStatus(null, "71898");
+        assertEquals("3.7.1", result.getSpigotPluginVersion());
     }
 }
